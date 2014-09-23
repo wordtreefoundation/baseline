44a4fff89878db81d0605589022cb1e26c26a522
Duane Johnson <duane.johnson@gmail.com>
2014-09-23 13:48:33 -0600

    Use two threads; add metadata to json

--- baseline.rb
+++ baseline.rb
@@ -2,12 +2,13 @@ require 'wordtriez'
 require 'wordtree'
 require 'optparse'
 require 'thread'
-require 'celluloid'
-require 'irb'
+# require 'irb'
+require 'json'
 
 options = {
   :library => "library",
-  :refs => false
+  :refs => false,
+  :output => "baseline.json"
 }
 
 OptionParser.new do |opts|
@@ -19,6 +20,10 @@ OptionParser.new do |opts|
 
   opts.on("-l", "--library LIBRARY", "Set library path to LIBRARY") do |path|
     options[:library] = path
+  end
+
+  opts.on("-o", "--output BASELINE", "Set baseline file path to BASELINE") do |path|
+    options[:output] = path
   end
 
   opts.on(nil, "--[no-]refs", "Include references to books (uses a lot of memory)") do |bool|
@@ -74,39 +79,57 @@ end
 case_study = $lib.find_without_ngrams(case_id)
 # $pool = TextWorker.pool(args: [case_study.content, options])
 
+worker = TextWorker.new(case_study.content, options)
 
-work_q = Queue.new
+content_q = Queue.new
 
 ref_id = 0
-$lib.library.each do |path, id|
-  ref_id += 1
-  work_q.push([id, path, ref_id])
+
+start = Time.now
+
+io_thread = Thread.new do
+  $lib.library.each do |path, id|
+    ref_id += 1
+    content_q.push([id, path, ref_id])
+  end
 end
 
-threads = (0...Celluloid.cores).map do
-  Thread.new do
-    puts "Creating Worker"
-    Thread.current[:worker] = worker = TextWorker.new(case_study.content, options)
-    begin
-      while args = work_q.pop(true)
-        worker.count_ngrams(*args)    
-      end
-    rescue ThreadError
+work_thread = Thread.new do
+  sleep 0.01 while ref_id == 0
+  begin
+    while args = content_q.pop(true)
+      worker.count_ngrams(*args)    
     end
+  rescue ThreadError
   end
 end
 
-threads.map(&:join)
+io_thread.join
+work_thread.join
 
-$master_trie = Wordtriez.new
-threads.each do |t|
-  puts "Size: #{t[:worker].trie.size}"
-  t[:worker].trie.each do |k, v|
-    # puts k, v
-    $master_trie[k] += v
-  end
-end
+finish = Time.now
 
 puts "Done."
 
-IRB.start
+meta =   {
+  "_meta" => {
+    "description" => "Baseline word frequencies for unigrams to 4-grams of words in the book #{case_id}",
+    "book_count" => ref_id,
+    "book_range_start_year" => 1750,
+    "book_range_end_year" => 1860,
+    "processing_time_in_seconds" => finish - start,
+    "processing_time_avg_per_book" => (finish - start).to_f / ref_id
+  }
+}
+
+File.open(options[:output], "w") do |file|
+  file.puts "{"
+  file.write JSON.pretty_generate(meta).split("\n").to_a[1..-2].join("\n") + ",\n"
+  worker.trie.each do |k, v|
+    file.puts "  \"#{k}\": #{v},"
+  end
+  file.puts "}"
+end
+
+# $t = worker.trie
+# IRB.start
